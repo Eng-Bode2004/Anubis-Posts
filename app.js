400998071bf48dd4a1920498ee9812a18997818c
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-10-28 04:24:03 +0300

    init commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const MongoDB = require('./Config/DataBase');
 const mongoose = require("mongoose"); // Data Base Configrations
 const express = require('express');
-
+const cors = require('cors');
 
 
 // Set Up Port and Make Server listen To requests
@@ -10,6 +10,13 @@ const app = express();
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 app.use(express.json()); // Middleware to parse JSON
+
+// ✅ حل مشكلة CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 // Users Routes //
 const Posts_Routes = require('./Routes/Post_Routes');
